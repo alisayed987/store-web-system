9467dff48d72409f3c08bce5604d9bdc7531f71a
ali.sayed987 <ali.sayed987@yahoo.com>
2023-06-30 12:38:44 +0200

    store door

---
+++ back-end/database/seeders/db_seeder.js
@@ -0,0 +1,8 @@
+// (async () => {
+//     await sequelize.sync();
+//     const jane = await Item.create({
+//       username: 'janedoe',
+//       birthday: new Date(1980, 6, 20)
+//     });
+//     console.log(jane.toJSON());
+//   })();
---
+++ store-door/src/components/Login.js
@@ -0,0 +1,10 @@
+import { Button } from 'react-bootstrap';
+
+function Login() {
+    return (
+        <div>
+            Login
+        </div>
+    );
+}
+export default Login;

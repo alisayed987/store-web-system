c2b8fec321a57a1540b499eda348d8360b03aa48
Yssff007 <youssefhussien116@gmail.com>
2023-07-14 15:02:01 +0200

    init frontend files

---
+++ store-door/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <footer className='bg-dark py-2 mt-5 text-center text-white start-0 end-0 fixed-bottom'>
+      <div>
+        <i className='fab fa-facebook-square p-2'></i> 
+        <i className='fab fa-linkedin p-2'></i>
+        <i className='fab fa-instagram p-2'></i>
+      </div>
+      
+      <h6> &copy; All Rights Reserved </h6>
+    </footer>
+    </>
+  )
+}

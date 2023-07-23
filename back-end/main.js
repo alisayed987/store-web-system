fdae915c5122e6b69defd71f43e1a6ecfc0d043a
ali.sayed987 <ali.sayed987@yahoo.com>
2023-07-23 17:05:40 +0200

    routes files structure

--- back-end/main.js
+++ back-end/main.js
@@ -3,19 +3,12 @@ const app = express();
 require('dotenv').config();
 
 const db = require('./database/db');
+require('./routes/routes')(app, db);
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
-});
 
 port = process.env.APP_PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Furniture store app listening at http://localhost:${port}`);
 });
+
+module.exports = server;

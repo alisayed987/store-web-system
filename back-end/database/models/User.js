79fe63bb39aef39c89b5f38e26a33c9fec0f68bd
ali.sayed987 <ali.sayed987@yahoo.com>
2023-08-07 01:29:31 +0200

    add phone number to user

--- back-end/database/models/User.js
+++ back-end/database/models/User.js
@@ -7,29 +7,44 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       this.hasMany(models.UserRole)
     }
+
+    generateAuthToken() {
+      console.log(this);
+      const token = jwt.sign({ id: this.id }, process.env.SECRET);
+      return token;
+    }
   }
   User.init({
     id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
-          }
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true
+      }
     },
     password: {
-        type: DataTypes.STRING(64),
-        allowNull: false,
+      type: DataTypes.STRING(64),
+      allowNull: false,
     }
   }, {
     sequelize,
--- back-end/database/seeders/20230720230609-users.js
+++ back-end/database/seeders/20230720230609-users.js
@@ -7,12 +7,14 @@ module.exports = {
       name: "Ali Sayed",
       email: "ali.sayed11298@gamil.com",
       password: "ali12345",
+      phone_number: "01149908553",
       created_at: new Date().toISOString().slice(0, 19).replace('T', ' '),
       updated_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
     },{
       name: "Youssef Hussien",
       email: "youssefhussien116@gmail.com",
       password: "ysf12345",
+      phone_number: "01143128579",
       created_at: new Date().toISOString().slice(0, 19).replace('T', ' '),
       updated_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
     }], {});
